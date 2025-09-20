0fa8c8c33f0a9c762abff661a4ce889e3002a974
Manavraj Thakor <manavraj.thakor@audviklabs.com>
2025-09-20 16:17:11 +0530

    Added  cors

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 model = joblib.load('xgb_model_final.pkl')
 
 # Airport/landmark coordinates
